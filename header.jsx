86c89f11f90cdc2abb19ee31b3453f5d5fcbd979
Hareem Fatima <129002691+hareem2806@users.noreply.github.com>
2024-05-26 20:05:59 +0500

    Add files via upload

--- header.jsx
+++ header.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
-import { AiOutlineMenuUnfold } from "react-icons/ai";
-import { AiOutlineClose } from "react-icons/ai";
-import logo from './logo.png';
-import pic from './picture.png';
+import { AiOutlineMenuUnfold, AiOutlineClose } from 'react-icons/ai';
+import logo from './logo.svg';
+
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
 
@@ -16,26 +16,20 @@ const Navbar = () => {
   };
 
   return (
-    <>
-    {/* <div className="header">
-    <img src={pic} alt="Header Image" className="header-image" />
-     </div> */}
     <nav className="navbar">
       <div className="navbar-brand">
         <img src={logo} alt="Logo" className="logo" />
       </div>
       <div className={`navbar-links ${menu ? 'active' : ''}`}>
-        <a href="#about" onClick={closeMenu}>About</a>
-        <a href="#diseases" onClick={closeMenu}>Diseases</a>
-        <a href="#blogs" onClick={closeMenu}>Blogs</a>
-        <a href="#reviews" onClick={closeMenu}>Reviews</a>
+        <Link to="/" onClick={closeMenu}>Home</Link>
+        <Link to="/diseases" onClick={closeMenu}>Diseases</Link>
+        <Link to="/menu" onClick={closeMenu}>Menu</Link>
       </div>
       <div className="menu-icon" onClick={toggleMenu}>
         {menu ? <AiOutlineClose size={25} /> : <AiOutlineMenuUnfold size={25} />}
       </div>
     </nav>
-    
- </> )
+  );
 };
 
 export default Navbar;
