d0fccd71df4762e8d8bbc3b6cb2339a1ebb047cb
Onaiza-Reaz <142627072+Onaiza-Reaz@users.noreply.github.com>
2024-05-27 11:53:33 +0500

    Create Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+const Navber =() => {
+    return <nav>
+        <div className="menu-icon">
+            <ul className="menu-list">
+
+                <li> 
+                <NavLink to="/">Home</NavLink>
+                </li>
+
+                <li> 
+                <NavLink to="/disease">Disease</NavLink>
+                </li>
+
+                <li> 
+                <NavLink to="/menu">Menu</NavLink>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+};
