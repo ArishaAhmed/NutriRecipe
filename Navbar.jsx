c0553d9d35c41427a84f04a80e5ca051ea984187
Onaiza-Reaz <142627072+Onaiza-Reaz@users.noreply.github.com>
2024-05-30 17:01:32 +0500

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,25 +1,50 @@
+// import React from "react";
+// import { NavLink } from 'react-router-dom';
+
+// const Navbar =() => {
+//     return <nav>
+//         <div className="menu-icon">
+//             <ul className="menu-list">
+
+//                 <li> 
+//                 <NavLink to="/">Home</NavLink>
+//                 </li>
+
+//                 <li> 
+//                 <NavLink to="/blogs">Blogs</NavLink>
+//                 </li>
+
+//                 <li> 
+//                 <NavLink to="/diseases">Disease</NavLink>
+//                 </li>
+
+//                 <li> 
+//                 <NavLink to="/menu">Menu</NavLink>
+//                 </li>
+
+//             </ul>
+//         </div>
+//     </nav>
+
+// };
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
-const Navbar =() => {
-    return <nav>
-        <div className="menu-icon">
-            <ul className="menu-list">
-
-                <li> 
-                <NavLink to="/">Home</NavLink>
-                </li>
-
-                <li> 
-                <NavLink to="/disease">Disease</NavLink>
-                </li>
-
-                <li> 
-                <NavLink to="/menu">Menu</NavLink>
-                </li>
-
-            </ul>
-        </div>
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="menu-icon">
+        <ul className="menu-list">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/diseases">Disease</NavLink></li>
+          <li><NavLink to="/menu">Menu</NavLink></li>
+          <li><NavLink to="/blogs">Blogs</NavLink></li>
+          <li><NavLink to="/recipe">Recipe</NavLink></li>
+          <li><NavLink to="/login">Login</NavLink></li>
+        </ul>
+      </div>
     </nav>
-
+  );
 };
+
+export default Navbar;
