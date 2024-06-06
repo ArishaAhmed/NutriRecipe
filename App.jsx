08c0933ad10cf7465591ca6c1a1f2ccb1b0a8ad0
Arisha Ahmed <148933778+ArishaAhmed@users.noreply.github.com>
2024-06-06 19:14:48 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import DiseasePage from './Diseases.jsx';
 import HomePage from './Home.jsx';
 import MenuPage from './Menu.jsx';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
-import BlogsPage from './Blogs.jsx';
 import Recipe from './Recipe.jsx';
-import Login from './login.jsx';
-import CreateAccount from './CreateAccount.jsx';
+import BlogsPage from './Blogs.jsx';
+import LoginForm from './LoginForm.jsx';
+import RegisterForm from './RegisterForm.jsx';
+import ProfilePage from './profilepage.jsx';
+import MenuRecipe from './menu_recipe.jsx';
+import RecipeDisplay from './RecipeDisplay.jsx';
+import ScrollToTop from './ScrollToTop'; // Importing ScrollToTop
 
 const Layout = ({ children }) => {
   const location = useLocation();
 
   // Define routes where the Header and Footer should not be displayed
-  const noHeaderFooterRoutes = ['/login', '/create-account'];
+  const noHeaderFooterRoutes = ['/login', '/register'];
 
   return (
     <>
@@ -26,17 +31,23 @@ const Layout = ({ children }) => {
 };
 
 const App = () => {
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
   return (
     <BrowserRouter>
+      <ScrollToTop /> {/* Adding ScrollToTop component */}
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/blogs" element={<BlogsPage />} />
           <Route path="/diseases" element={<DiseasePage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/recipe" element={<Recipe />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/create-account" element={<CreateAccount />} /> {/* Add this route */}
+          <Route path="/NutriRecipe" element={<MenuRecipe />} />
+          <Route path="/menu/:category" element={<MenuPage setSelectedRecipe={setSelectedRecipe} />} />
+          <Route path="/:category/:dish" element={<Recipe selectedRecipe={selectedRecipe} />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/recipe/:dish" element={<RecipeDisplay />} />
         </Routes>
       </Layout>
     </BrowserRouter>
